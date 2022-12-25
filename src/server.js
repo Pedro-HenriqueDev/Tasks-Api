d0663f38eb7fee6c9a05594a862eb3af1289e763
Pedro-HenriqueDev <ph1305803@gmail.com>
2022-12-24 22:35:04 -0300

    fix app.js, server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const app = require("./app")
 const PORT = process.env.PORT || 3000
 
-
 app.listen(PORT, (req,res) => {
     console.log("RODANDO NA PORTA " + PORT)
 });
